f6f27fbb97d2a87913475ff5ab6837d85d3b950e
alejandro largo <largoalejo@gmail.com>
2022-07-23 21:49:28 -0500

    first commit

---
+++ 03-counter-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h2>Hola mundo</h2>
+    </>
+  )
+}
+
+export default App
---
+++ 03-counter-app/src/FirstApp.jsx
@@ -0,0 +1,10 @@
+export default function FirstApp () {
+  const newMessage = 'Alejandro!!!'
+
+  return (
+    <>
+      <h1>{newMessage}</h1>
+      <p>Soy un subtitulo</p>
+    </>
+  )
+}
