a8e371da3b0a946cd635a2c79aefe9b2c221bdf0
alejandro largo <largoalejo@gmail.com>
2022-07-30 14:13:55 -0500

    un solo handle para sumar, restar y resetear

--- 03-counter-app/src/CounterApp.jsx
+++ 03-counter-app/src/CounterApp.jsx
@@ -3,15 +3,22 @@ import { PropTypes } from 'prop-types';
 
 export const CounterApp = ({ value }) => {
   const [counter, setCounter] = useState(value)
-  const handleAdd = () => {
-    setCounter(prevCounter => prevCounter + 1)
+  // const handleAdd = () => {
+  //   setCounter(prevCounter => prevCounter + 1)
+  // }
+
+  const handleChange = (change) => {
+    if (change === 0) return setCounter(0)
+    return setCounter(prevCounter => prevCounter + change)
   }
 
   return (
     <>
       <h1>Counter App</h1>
       <h2> {counter} </h2>
-      <button onClick={handleAdd}>+1</button>
+      <button onClick={() => handleChange(-1)}>-1</button>
+      <button onClick={() => handleChange(+1)}>+1</button>
+      <button onClick={() => handleChange(0)}>reset</button>
     </>
   )
 }
