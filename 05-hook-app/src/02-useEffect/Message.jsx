4bf5ecef3445a7d7a4644091947bb6c326ead694
alejandro largo <largoalejo@gmail.com>
2022-08-15 21:56:41 -0500

    warnings por no limpiar el useEffect

--- 05-hook-app/src/02-useEffect/Message.jsx
+++ 05-hook-app/src/02-useEffect/Message.jsx
@@ -2,10 +2,14 @@ import { useEffect } from 'react'
 
 export default function Message () {
   useEffect(() => {
-    console.log('Message mounted!')
+    const onMouseMove = ({ x, y }) => {
+      const coords = { x, y }
+      console.log(coords)
+    }
+    window.addEventListener('mousemove', onMouseMove)
 
     return () => {
-      console.log('Message unmounted!')
+      window.removeEventListener('mousemove', onMouseMove)
     }
   }, [])
   return (
